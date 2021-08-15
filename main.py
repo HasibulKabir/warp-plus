c5bb574c204fb62f867b44e2af046066053bebee
Md. Hasibul Kabir <46620128+HasibulKabir@users.noreply.github.com>
2021-08-15 15:23:34 +0600

    [ Black ] formatter and python version update.

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import traceback
 import threading
 import urllib.error
 import urllib.request
-from urllib.parse import parse_qs, urlparse
 from flask import Flask, jsonify
+from urllib.parse import parse_qs, urlparse
+
 
 app = Flask(__name__)
-app.config['TOTAL'] = 0
+app.config["TOTAL"] = 0
 referrerID = os.environ.get("REFERRER_ID")
 
 
@@ -27,50 +28,61 @@ class WarpyAPI:
 
     @staticmethod
     def __gen_random_string(n):
-        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(n))
+        return "".join(
+            random.choice(string.ascii_letters + string.digits)
+            for _ in range(n)
+        )
 
     @property
     def api_url(self):
-        return 'https://api.cloudflareclient.com/v0a{}/reg'.format(random.randint(100, 999))
+        return "https://api.cloudflareclient.com/v0a{}/reg".format(
+            random.randint(100, 999)
+        )
 
     def get_refferID(self) -> str:
         opener = urllib.request.build_opener(RedirectHandler)
         urllib.request.install_opener(opener)
         try:
-            resp = urllib.request.urlopen(os.environ.get("REFERRER_URL", "https://warp.plus/FRs9d"))
+            resp = urllib.request.urlopen(
+                os.environ.get("REFERRER_URL", "https://warp.plus/FRs9d")
+            )
         except urllib.error.HTTPError as e:
             resp = e
         url = resp.headers.get("location")
-        return parse_qs(urlparse(url).query)['referrer'][0]
+        return parse_qs(urlparse(url).query)["referrer"][0]
 
     def get_data(self):
         install_id = self.__gen_random_string(11)
 
         body = json.dumps(
             {
-                'key': '{}='.format(self.__gen_random_string(43)),
-                'install_id': install_id,
-                'fcm_token': '{}:{}'.format(install_id, self.__gen_random_string(140)),
-                'referrer': self.referrerID,
-                'warp_enabled': False,
-                'tos': datetime.datetime.now().astimezone().isoformat(),
-                'type': 'Android',
+                "key": "{}=".format(self.__gen_random_string(43)),
+                "install_id": install_id,
+                "fcm_token": "{}:{}".format(
+                    install_id, self.__gen_random_string(140)
+                ),
+                "referrer": self.referrerID,
+                "warp_enabled": False,
+                "tos": datetime.datetime.now().astimezone().isoformat(),
+                "type": "Android",
             }
-        ).encode('UTF-8')
+        ).encode("UTF-8")
 
         headers = {
-            'Host': 'api.cloudflareclient.com',
-            'User-Agent': 'okhttp/3.12.1',
-            'Content-Type': 'application/json',
+            "Host": "api.cloudflareclient.com",
+            "User-Agent": "okhttp/3.12.1",
+            "Content-Type": "application/json",
         }
 
         try:
-            req = urllib.request.Request(self.api_url, headers=headers, data=body)
+            req = urllib.request.Request(
+                self.api_url, headers=headers, data=body
+            )
             response = urllib.request.urlopen(req)
-            con = response.read().decode('UTF-8')
+            con = response.read().decode("UTF-8")
             if response.getcode() == 200 and self.referrerID in con:
-                app.config['TOTAL'] += 1
-                print('Total: {}GB... Earned 1GB.'.format(app.config['TOTAL']))
+                app.config["TOTAL"] += 1
+                print("Total: {}GB... Earned 1GB.".format(app.config["TOTAL"]))
 
         except Exception:
             traceback.print_exc()
@@ -81,20 +93,17 @@ class WarpyAPI:
             time.sleep(int(os.environ.get("WAIT_TIME", 20)))
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return jsonify(
-        {
-            "status": "ok",
-            "sended": app.config['TOTAL']
-        }
-    )
+    return jsonify({"status": "ok", "sended": app.config["TOTAL"]})
+
 
 thread = threading.Thread(target=WarpyAPI().start, daemon=True)
 thread.start()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("HOST", "0.0.0.0"),
-        port=os.environ.get("PORT", 5000)
+        port=os.environ.get("PORT", 5000),
     )
